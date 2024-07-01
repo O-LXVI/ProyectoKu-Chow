a8c61fb19669dd03e0a8136c9e888f7a9fdcca2a
toru <venegasvp08@gmail.com>
2024-06-30 23:44:57 -0400

    django agregado

--- kuchauproject/polls/urls.py
+++ kuchauproject/polls/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import ItemViewSet
 
+router = DefaultRouter()
+router.register(r'items', ItemViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("admin/", admin.site.urls)
+    path("admin/", admin.site.urls),
+    path("crud", views.crud, name="crud")
 ]
